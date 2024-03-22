2615fe2f8e5406db058ecc6ffcf2926dd0660614
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-03-22 18:17:47 -0500

    fixed sticky navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ $(function() {
     $(window).on('scroll', function (event) {
         var currentPage = window.location.href;
         var scroll = $(window).scrollTop();
-        if(currentPage.indexOf("index.html") !== -1){
+        if(currentPage.indexOf("postmortem.html") === -1){
             if (scroll < 20) {
                 $(".header_navbar").removeClass("sticky");
                 $(".header_navbar img").attr("src", "assets/images/navlogowhite.svg");
@@ -31,17 +31,19 @@ $(function() {
     var scrollLink = $('.page-scroll');
     // Active link switching
     $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
+        if(scrollLink.length){
+            var scrollbarLocation = $(this).scrollTop();
 
-        scrollLink.each(function () {
-
-            var sectionOffset = $(this.hash).offset.top - 73;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
+            scrollLink.each(function () {
+    
+                var sectionOffset = $(this.hash).offset().top - 73;
+    
+                if (sectionOffset <= scrollbarLocation) {
+                    $(this).parent().addClass('active');
+                    $(this).parent().siblings().removeClass('active');
+                }
+            });
+        }
     });
     
     //===== close navbar-collapse when a  clicked
